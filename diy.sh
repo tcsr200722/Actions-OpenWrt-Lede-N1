28bf4334d81cf126a1b5960cebc324c3b18502a4
roacn <84313163+roacn@users.noreply.github.com>
2021-06-03 01:52:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -54,8 +54,8 @@ git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 
 # Add luci-theme-argon
-# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-# rm -rf ../lean/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
 
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git
 rm -rf ./openwrt-packages/luci-app-adguardhome
